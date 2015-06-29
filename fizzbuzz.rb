58354650a8d1ad087882215df700959305809bc2
Kohl Kohlbrenner <edwin.kohlbrenner@gmail.com>
2015-06-29 10:28:33 -0400

    rubyquiz fizzbuzz

---
+++ fizzbuzz.rb
@@ -0,0 +1,12 @@
+
+if ARGV[0].class === 'Number' && ARGV.length == 1 && ARGV[0].to_i > 0
+   max = ARGV[0].to_i
+
+   1.upto(max) do |num|
+    (num % 5 == 0 && num % 3 == 0) ? (puts "Fizzbuzz") : ((num % 5 == 0) ? (puts "Fizz"):((num % 3 == 0) ? (puts "Buzz") : (puts num)))
+   end
+   puts "All Done"
+else
+  print "Please include a number greater than 0 as a file argument\n"
+  false
+end
